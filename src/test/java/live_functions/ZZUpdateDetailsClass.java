c4248cf3c18897a3f89f35c68c26f760d2e32e37
BHUPESH <BHUPESH@DESKTOP-T6L47BI>
2024-11-18 18:49:46 +0530

    Details updated

--- src/test/java/live_functions/ZZUpdateDetailsClass.java
+++ src/test/java/live_functions/ZZUpdateDetailsClass.java
@@ -3,7 +3,7 @@ package live_functions;
 public class ZZUpdateDetailsClass {
 	
 	public static void main(String[] args) {
-		System.out.println("14 nov 2024");
+		System.out.println("git padate dae- 16 nov 2024");
 	}
 
 }
