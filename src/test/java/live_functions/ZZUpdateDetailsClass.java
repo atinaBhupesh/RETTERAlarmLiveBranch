12c712f901ffe999e3616ed8d70313ab6a0d61da
atinaBhupesh <142960186+atinaBhupesh@users.noreply.github.com>
2025-06-13 16:01:43 +0530

    update-09/06/2025-0200

--- src/test/java/live_functions/ZZUpdateDetailsClass.java
+++ src/test/java/live_functions/ZZUpdateDetailsClass.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 public class ZZUpdateDetailsClass {
 	
 	public static void main(String[] args) {
-		System.out.println("update-09/06/2025-02");
+		System.out.println("update-09/06/2025-0200");
 		
 		
 	}
